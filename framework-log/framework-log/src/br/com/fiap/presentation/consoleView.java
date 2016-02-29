568bf04f2ad7fc373ad47f43ff3b0505bea109f6
cesarnobre <cesarnobrefilho@gmail.com>
2016-02-29 20:32:42 -0300

    added more logs

--- framework-log/framework-log/src/br/com/fiap/presentation/consoleView.java
+++ framework-log/framework-log/src/br/com/fiap/presentation/consoleView.java
@@ -1,13 +1,19 @@
 package br.com.fiap.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.fiap.bo.VeiculoBO;
 
 public class ConsoleView {
-
+	public static final Logger log = LoggerFactory.getLogger(VeiculoBO.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		log.trace("Iniciando a aplicação");
 		VeiculoBO bo = new VeiculoBO();
 		bo.cadastrar(null);
+		log.info("Finalizando a aplicação");
 	}
 
 }
