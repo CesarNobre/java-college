9a663ad8e9171cca557cccd0926430be125512ef
cesarnobre <cesarnobrefilho@gmail.com>
2016-02-29 22:32:53 -0300

    finished basic example soap webservice java

---
+++ webservice-provider/src/br/com/fiap/services/CotacaoBO.java
@@ -0,0 +1,11 @@
+package br.com.fiap.services;
+
+public class CotacaoBO {
+	public double ConverterEuroReal(double euro){
+		return euro*4.5;
+	}
+	
+	public double converterDolarReal(double dolar){
+		return dolar*4.1;
+	}
+}
