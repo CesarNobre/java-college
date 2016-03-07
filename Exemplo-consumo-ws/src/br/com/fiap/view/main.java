33142daebb27042260b4c84984d73365e44a949e
cesarnobre <cesarnobrefilho@gmail.com>
2016-03-07 20:59:00 -0300

    added correios call

--- Exemplo-consumo-ws/src/br/com/fiap/view/main.java
+++ Exemplo-consumo-ws/src/br/com/fiap/view/main.java
@@ -2,6 +2,9 @@ package br.com.fiap.view;
 
 import javax.swing.JOptionPane;
 
+import org.tempuri.CalcPrecoPrazoWSStub;
+import org.tempuri.CalcPrecoPrazoWSStub.CalcPrecoPrazo;
+
 import br.com.fiap.bo.EstoqueBOStub;
 import br.com.fiap.bo.EstoqueBOStub.BuscarProduto;
 import br.com.fiap.bo.EstoqueBOStub.BuscarProdutoResponse;
@@ -25,6 +28,25 @@ public class main {
 			System.out.println(produto.getAltura());
 			System.out.println(produto.getLargura());
 			System.out.println(produto.getTipo());
+ 
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	private static void CorreiosWS(){
+		try {
+			CalcPrecoPrazoWSStub ws = new CalcPrecoPrazoWSStub();
+			CalcPrecoPrazo param = new CalcPrecoPrazo();
+			param.setNCdEmpresa("123");
+			param.setSDsSenha("123");
+			param.setNCdServico("1");
+			param.setSCepOrigem("04349-000");
+			param.setSCepDestino("04349-000");
+			param.setNVlPeso("30");
+			param.setNCdFormato(1);
+			
+
 
 		} catch (Exception e) {
 			// TODO: handle exception
